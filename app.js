b67c204fc2428d81b759df4d37aa7b1652b6635e
Artur Shapiro <arturshapiro@gmail.com>
2022-05-08 17:18:43 -0500

    .

--- app.js
+++ app.js
@@ -1,38 +1,40 @@
-// Add this to your javascript file
 
-/* Body */
+let light = localStorage.getItem('light')
+let dawn = localStorage.getItem('dawn')
+let dark = localStorage.getItem('dark');
+
 const body = document.querySelector('body');
+const lightButton = document.getElementById('light')
+const darkButton = document.getElementById('dark')
+const dawnButton = document.getElementById('dawn')
 
-// Dark Mode Action
-let darkMode = localStorage.getItem("darkMode");
-const darkModeToggle = document.querySelector('.dark-mode-button');
-const darkModeToggleFooter = document.querySelector('footer .dark-mode-button');
+const theme = localStorage.getItem('theme')
 
-// Enable Dark Mode
-const enableDarkMode = () => {
-    body.classList.add("dark-mode");
-    localStorage.setItem("darkMode", "enabled")
+
+if (theme) {
+    body.classList.add('theme')
 }
 
-// Disable Dark Mode
-const disableDarkMode = () => {
-    body.classList.remove("dark-mode");
-    localStorage.setItem("darkMode", null)
+
+lightButton.onClick = () => {
+    body.classList.replace('dark', 'light')
+    localStorage.setItem('theme', 'light')
+
+darkButton.onClick = () => {
+    body.classList.replace('light', 'dark ')
+    localStorage.setItem('theme', 'dark')
 }
 
-if (darkMode == "enabled") {
-    enableDarkMode();
-}
-
-// Desktop Button
-darkModeToggle.addEventListener('click', () => {
-    darkMode = localStorage.getItem("darkMode");
-    if (darkMode !== "enabled") {
-        enableDarkMode();
+dawnButton.onClick = () => {
+    if (body.classList.contains('dawn')) {
+        body.classList.remove('dawn')
+        localStorage.removeItem('dawn')
     } else {
-        disableDarkMode();
+        body.classList.add('dawn')
+        localStorage.setItem('dawn', true)
     }
-})
+}
+
 
 // Footer button, optional. This is for if you have a second dark mode toggle button
 //in the footer, just make sure the button is inside the footer tag, and it will be
