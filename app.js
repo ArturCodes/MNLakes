db12b54856f5eadea257585d84922d003901ee3e
Artur Shapiro <arturshapiro@gmail.com>
2022-05-08 22:47:26 -0500

    .
    .

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
 
-let light = localStorage.getItem('light')
-let dawn = localStorage.getItem('dawn')
-let dark = localStorage.getItem('dark');
 
-const body = document.querySelector('body');
 const lightButton = document.getElementById('light')
 const darkButton = document.getElementById('dark')
 const dawnButton = document.getElementById('dawn')
 
+const body = document.body  ;
 const theme = localStorage.getItem('theme')
 
 
@@ -16,35 +13,27 @@ if (theme) {
 }
 
 
-lightButton.onClick = () => {
-    body.classList.replace('dark', 'light')
-    localStorage.setItem('theme', 'light')
+lightButton.onclick = () => {
+    body.classList.remove(...body.classList)
+    body.classList.add('light')
 
-darkButton.onClick = () => {
-    body.classList.replace('light', 'dark ')
-    localStorage.setItem('theme', 'dark')
+    localStorage.setItem('theme', 'light');
 }
 
-dawnButton.onClick = () => {
-    if (body.classList.contains('dawn')) {
-        body.classList.remove('dawn')
-        localStorage.removeItem('dawn')
-    } else {
-        body.classList.add('dawn')
-        localStorage.setItem('dawn', true)
-    }
+darkButton.onclick = () => {
+    body.classList.remove(...body.classList)
+    body.classList.add('dark')
+
+    localStorage.setItem('theme', 'dark');
+}
+
+dawnButton.onclick = () => {
+    body.classList.remove(...body.classList)
+    body.classList.add('dawn') 
+
+    localStorage.setItem('dawn', true)
+    
 }
 
 
-// Footer button, optional. This is for if you have a second dark mode toggle button
-//in the footer, just make sure the button is inside the footer tag, and it will be
-//linked to this function.
-
-    darkModeToggleFooter.addEventListener('click', () => {
-        darkMode = localStorage.getItem("darkMode");
-        if (darkMode !== "enabled") {
-            enableDarkMode();
-        } else {
-            disableDarkMode();
-        }
-    })
+ 
